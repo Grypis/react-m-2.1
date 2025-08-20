4d5bbce5e66dbb2b8cbd3b9764f67688a14c92d3
grypis <grypisnecros@gmail.com>
2025-08-20 21:47:07 +0300

    asd

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 import { useState } from "react";
-import { ClickCounter } from "./ClickCounter";
+// import { ClickCounter } from "./ClickCounter";
 
 /* const CustomButton = ({ message, children }) => {
   return <button onClick={() => alert(message)}>{children}</button>;
 }; */
 
-const App = () => {
-  /*  const handleClick = () => {
+// const App = () => {
+/*  const handleClick = () => {
     alert("I`m a button!");
   };
   return <button onClick={handleClick}>Click me!</button>; */
-  /* return <button onClick={() => alert("I'm a button!")}>Click me!</button>; */
+/* return <button onClick={() => alert("I'm a button!")}>Click me!</button>; */
 
-  /* const handleClick = (evt) => {
+/* const handleClick = (evt) => {
     console.log(evt);
   };
   return (
@@ -22,14 +22,14 @@ const App = () => {
     </>
   ); */
 
-  /* return (
+/* return (
     <>
       <CustomButton message="Playing music!">Playing music!</CustomButton>
       <CustomButton message="Uploading your data!">Upload data</CustomButton>
     </>
   ); */
 
-  /* // let clicks = 0;
+/* // let clicks = 0;
   const [clicks, setClicks] = useState(0);
   const [isOpen, setIsOpen] = useState(false);
   const handleClick = () => {
@@ -46,13 +46,26 @@ const App = () => {
       {isOpen && <p>Now you can see me!</p>}
     </>
   ); */
+
+/*  const ClickCounter = () => {
+    return <button onClick={handleClick}>Current: {clicks}</button>;
+  };
+
+  const App = () => {
+    const [clicks, setClicks] = useState(0);
+
+    const handleClick = () => {
+      setClicks(clicks + 1);
+    };
+  };
+
   return (
     <>
       <ClickCounter />
       <ClickCounter />
     </>
   );
-};
+}; */
 
 /* function App() {
   const handleClick = (evt) => {
@@ -66,5 +79,68 @@ const App = () => {
     </>
   );
 } */
+
+/* const ClickCounter = ({ value, onUpdate }) => {
+  return <button onClick={onUpdate}>Current: {value}</button>;
+};
+
+const App = () => {
+  const [clicks, setClicks] = useState({
+    X: 0,
+    y: 0,
+  });
+
+  const updateX = () => {
+    setValues({
+      ...value,
+      x: values.x + 1,
+    });
+  };
+
+  const updateY = () => {};
+
+  return (
+    <div>
+      <p>
+        x: {values.x}, y: {values.y}
+      </p>
+
+      <button onClick={updateX}>Update x</button>
+      <button onClick={updateY}>Update y</button>
+    </div>
+  );
+}; */
+
+const App = () => {
+  const [values, setValues] = useState({
+    x: 0,
+    y: 0,
+  });
+
+  const updateX = () => {
+    setValues({
+      ...values,
+      x: values.x + 1,
+    });
+  };
+
+  const updateY = () => {
+    setValues({
+      ...values,
+      y: values.y + 1,
+    });
+  };
+
+  return (
+    <div>
+      <p>
+        x: {values.x}, y: {values.y}
+      </p>
+
+      <button onClick={updateX}>Update x</button>
+      <button onClick={updateY}>Update y</button>
+    </div>
+  );
+};
 
 export default App;
